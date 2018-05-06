dd628ccf46729936a82f2a60ea24617252a6069e
Azat Akhmetshin <raakhme@gmail.com>
2018-05-06 23:56:48 +0300

    Правки для деплоя на heroku

--- webpack/prod.js
+++ webpack/prod.js
@@ -18,11 +18,21 @@ function getConfig () {
 
 		devtool: "cheap-source-map",
 
+		node: {
+			fs: 'empty',
+			net: 'empty'
+		},
+
 		output: {
 			publicPath: "/"
 		},
 
 		plugins: [
+			new webpack.DefinePlugin({
+				"process.env": {
+					NODE_ENV: JSON.stringify("production")
+				}
+			}),
 			new plugins.babelEnginePlugin({
 				presets: ['env', 'es2015', 'react', 'stage-0', 'stage-1']
 			})
